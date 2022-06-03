625e2f1832aa78b0b0f3960f5a8fc9a35fbefbe7
Andrii Pedosenko <andrey137217@gmail.com>
2022-06-03 20:47:03 +0300

    Added: -BottomNavigationView -4 empty fragments -day and night themes

--- app/src/main/java/com/example/filmshelper/MainActivity.kt
+++ app/src/main/java/com/example/filmshelper/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.filmshelper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.filmshelper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //val controller = findNavController(R.id.fragmentContainerView)
+
+        //binding.bottomNavigationView.setupWithNavController()
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
+
 }
